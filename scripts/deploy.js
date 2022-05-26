3a5f6982e97576919924292cb1e699871ee5db35
Viktor <viktor.m.rumenov@gmail.com>
2022-05-26 15:04:24 +0200

    Added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+const main = () => {
+  const Todo = await ethers.getContractFactory('Todo');
+  const todo = await Todo.deploy();
+  await todo.deployed();
+  console.log(`Contract deployed to: ${todo.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
